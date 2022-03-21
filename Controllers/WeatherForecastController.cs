9b4b54a9f1217bfb66f21321ecdc572a27aeb5de
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2022-03-21 10:26:18 -0300

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://httpbin.org/")
+            };
+
+            var response = await client.SendAsync(
+                new HttpRequestMessage(HttpMethod.Get, "get")
+                {
+                    Version = HttpVersion.Version20,
+                });
+
+            if (response.IsSuccessStatusCode)
+                return Ok(response.Content);
+
+            return BadRequest();
+        }
+    }
+}
